be4c3e637d6bab2dca22c51af7ee440ceb9e687c
Kai I <g9mailbox@gmail.com>
2022-09-16 15:09:43 -0400

    first attemp

---
+++ src/WarmUp.java
@@ -0,0 +1,13 @@
+import java.util.Map;
+
+public class WarmUp {
+    public static void main(String[] args) {
+        String[] strArr = {"White", "Black", "Green", "Red", "Yellow"};
+        Map<String, Integer> resMap = getColorsLen(strArr);
+        System.out.println(resMap);
+    }
+
+    public static Map<String, Integer> getColorsLen(String[] strArr) {
+        return null;
+    }
+}
